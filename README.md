1c2919f80715fbd5dca0fa0dba2cdc5fee09d0b7
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-26 19:49:03 -0400

    build: setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# H manager
+
+The front end application of `H manager` hotel management system.
---
+++ src/app/App.tsx
@@ -0,0 +1,5 @@
+import {JSX} from "react";
+
+export function App(): JSX.Element {
+    return <><h1>H manager</h1></>
+}

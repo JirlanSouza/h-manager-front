3076e04b1bf8d6c9aeb8716346cc5439db6ff1dc
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-28 22:29:57 -0400

    feat: created the app `SettingsService` class

---
+++ src/app/service/settingsService.ts
@@ -0,0 +1,23 @@
+import { COLOR_MODE } from "../theme/theme";
+
+export class SettingsService {
+    private readonly COLOR_MODE_KEY = "app_color_mode";
+    private readonly DRAWER_OPEN_KEY = "app_drawer_open";
+
+    getColorMode(): COLOR_MODE {
+        return (localStorage.getItem(this.COLOR_MODE_KEY) ||
+            COLOR_MODE.LIGHT) as COLOR_MODE;
+    }
+
+    saveColorMode(mode: COLOR_MODE) {
+        localStorage.setItem(this.COLOR_MODE_KEY, mode);
+    }
+
+    getDrawerOpen(): boolean {
+        return Boolean(localStorage.getItem(this.DRAWER_OPEN_KEY));
+    }
+
+    saveDrawerOpen(drawerOpen: boolean) {
+        localStorage.setItem(this.DRAWER_OPEN_KEY, String(drawerOpen));
+    }
+}

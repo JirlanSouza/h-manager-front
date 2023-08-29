4ca7349886bd6e95fcc70597f1cee9e15d994293
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-28 22:28:08 -0400

    feat: created the app theme and component style customixation

---
+++ src/app/theme/baseComponentsStyles.ts
@@ -0,0 +1,84 @@
+import { Theme } from "@mui/material/styles";
+
+export function baseComponentStylesCustomizer(theme: Theme) {
+    theme.components = {
+        MuiAppBar: {
+            styleOverrides: {
+                root: {
+                    background: theme.palette.background.paper,
+                    color: theme.palette.getContrastText(
+                        theme.palette.background.paper
+                    ),
+                },
+            },
+        },
+        MuiIconButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: theme.shape.borderRadius,
+                    border: `1px solid ${theme.palette.divider}`,
+                    background: theme.palette.action.hover,
+                    color: theme.palette.primary.main,
+                    "&:hover": {
+                        background: theme.palette.action.selected,
+                        color: theme.palette.primary.dark,
+                    },
+                },
+            },
+        },
+        MuiListItemButton: {
+            styleOverrides: {
+                root: {
+                    color: theme.palette.text.secondary,
+                    borderRadius: theme.shape.borderRadius,
+                    "&.Mui-selected": {
+                        color: theme.palette.primary.dark,
+                        "& .MuiListItemIcon-root": {
+                            color: theme.palette.primary.dark,
+                        },
+                        "& .MuiListItemText-root": {
+                            color: theme.palette.primary.dark,
+                        },
+                    },
+                    "&:hover": {
+                        color: theme.palette.primary.dark,
+                        "& .MuiListItemIcon-root": {
+                            color: theme.palette.primary.dark,
+                        },
+                        "& .MuiListItemText-root": {
+                            color: theme.palette.primary.dark,
+                        },
+                    },
+                },
+            },
+        },
+        MuiListItemIcon: {
+            styleOverrides: {
+                root: {
+                    color: theme.palette.text.secondary,
+                },
+            },
+        },
+        MuiListItemText: {
+            styleOverrides: {
+                primary: {
+                    color: theme.palette.text.primary,
+                },
+            },
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: {
+                    "&:hover ": {
+                        "& .MuiOutlinedInput-notchedOutline": {
+                            borderColor: theme.palette.primary.main,
+                        },
+                    },
+                    "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: theme.palette.divider,
+                    },
+                },
+            },
+        },
+    };
+}
---
+++ src/app/theme/theme.ts
@@ -0,0 +1,140 @@
+import { createTheme } from "@mui/material";
+import {
+    blue,
+    common,
+    green,
+    lightBlue,
+    orange,
+    purple,
+    red,
+} from "@mui/material/colors";
+import { baseComponentStylesCustomizer } from "./baseComponentsStyles";
+
+const palette = {
+    light: {
+        primary: {
+            main: blue[200],
+            light: blue[50],
+            dark: blue[400],
+        },
+        secondary: {
+            main: purple[200],
+            light: purple[50],
+            dark: purple[400],
+        },
+        error: {
+            main: red[500],
+            light: red[300],
+            dark: red[700],
+        },
+        info: {
+            main: lightBlue[400],
+            light: lightBlue[300],
+            dark: lightBlue[700],
+        },
+        success: {
+            main: green[400],
+            light: green[300],
+            dark: green[700],
+        },
+        warning: {
+            main: orange[400],
+            light: orange[300],
+            dark: orange[700],
+        },
+        text: {
+            primary: "rgba(0, 0, 0, 0.87)",
+            secondary: "rgba(0, 0, 0, 0.6)",
+            disabled: "rgba(0, 0, 0, 0.38)",
+        },
+        divider: "rgba(0, 52, 149, 0.12)",
+        background: {
+            paper: common.white,
+            default: common.white,
+        },
+        action: {
+            active: "rgba(144, 202, 249, 0.54)",
+            hover: "rgba(144, 202, 249, 0.08)",
+            hoverOpacity: 0.08,
+            selected: "rgba(144, 202, 249, 0.16)",
+            selectedOpacity: 0.16,
+            disabled: "rgba(144, 202, 249, 0.3)",
+            disabledBackground: "rgba(144, 202, 249, 0.12)",
+            disabledOpacity: 0.38,
+            focus: "rgba(144, 202, 249, 0.12)",
+            focusOpacity: 0.12,
+            activatedOpacity: 0.12,
+        },
+    },
+    dark: {
+        primary: {
+            main: blue[700],
+            light: blue[400],
+            dark: blue[800],
+        },
+        secondary: {
+            main: purple[500],
+            light: purple[300],
+            dark: purple[700],
+        },
+        error: {
+            main: red[700],
+            light: red[400],
+            dark: red[800],
+        },
+        info: {
+            main: lightBlue[700],
+            light: lightBlue[500],
+            dark: lightBlue[900],
+        },
+        success: {
+            main: green[800],
+            light: green[500],
+            dark: green[900],
+        },
+        warning: {
+            main: "#ed6c02",
+            light: orange[500],
+            dark: orange[900],
+        },
+        text: {
+            primary: common.white,
+            secondary: "rgba(255, 255, 255, 0.7)",
+            disabled: "rgba(255, 255, 255, 0.5)",
+            icon: "rgba(255, 255, 255, 0.5)",
+        },
+        divider: "rgb(31, 38, 48)",
+        background: {
+            paper: "#121212",
+            default: "#121212",
+        },
+        action: {
+            active: common.white, //rgba(25, 118, 210, 0.16)
+            hover: "rgba(25, 118, 210, 0.08)",
+            hoverOpacity: 0.08,
+            selected: "rgba(25, 118, 210, 0.16)",
+            selectedOpacity: 0.16,
+            disabled: "rgba(25, 118, 210, 0.3)",
+            disabledBackground: "rgba(25, 118, 210, 0.12)",
+            disabledOpacity: 0.38,
+            focus: "rgba(25, 118, 210, 0.12)",
+            focusOpacity: 0.12,
+            activatedOpacity: 0.24,
+        },
+    },
+};
+
+export enum COLOR_MODE {
+    LIGHT = `light`,
+    DARK = `dark`,
+}
+
+export const getAppTheme = (mode: COLOR_MODE) => {
+    const theme = createTheme({
+        palette: palette[mode],
+    });
+    theme.shape.borderRadius = parseInt(theme.spacing(1.5));
+    baseComponentStylesCustomizer(theme);
+
+    return theme;
+};

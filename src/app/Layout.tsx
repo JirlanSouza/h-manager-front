0c6d48a038703e061deb571c6268e4d4a55066f7
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-28 22:31:05 -0400

    feat: created the app `Layout` comonent

---
+++ src/app/Layout.tsx
@@ -0,0 +1,40 @@
+import { CssBaseline, useTheme } from "@mui/material";
+import Box from "@mui/material/Box";
+import { JSX, ReactNode, useState } from "react";
+import { AppBarComponent } from "./components/AppBarComponent.tsx";
+import { DrawerComponent } from "./components/DrawerComponent.tsx";
+import { MainContentComponent } from "./components/MainContentComponent.tsx";
+import { SettingsService } from "./service/settingsService.ts";
+
+interface LayoutProps {
+    children?: ReactNode;
+    settingService: SettingsService;
+}
+
+export function Layout({ children, settingService }: LayoutProps): JSX.Element {
+    const theme = useTheme();
+    const [open, setOpen] = useState(settingService.getDrawerOpen());
+
+    function handleDrawerOpen() {
+        setOpen((s) => {
+            settingService.saveDrawerOpen(!s);
+            return !s;
+        });
+    }
+
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                background: theme.palette.background.paper,
+            }}
+        >
+            <CssBaseline />
+            <AppBarComponent open={open} onMenuButtonClick={handleDrawerOpen} />
+            <DrawerComponent open={open} />
+            <MainContentComponent>{children}</MainContentComponent>
+        </Box>
+    );
+}

3ddb659eaa3cfbd4e27215844f9e068584f1ee0a
Jirlan Souza <jirlansouza08@gmail.com>
2023-09-03 15:35:51 -0400

    refactor: change the `AddCustomerScreen` to use app base components

--- src/screens/customers/AddCustomerScreen.tsx
+++ src/screens/customers/AddCustomerScreen.tsx
@@ -1,191 +1,125 @@
 import { CancelRounded, SaveRounded } from "@mui/icons-material";
-import {
-    Box,
-    Button,
-    Grid,
-    TextField,
-    Typography,
-    useTheme,
-} from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { Button } from "../../common/components/Button";
+import { Form } from "../../common/components/Form";
+import { FormFieldSet } from "../../common/components/FormFieldSet";
+import { FormInput } from "../../common/components/FormInput";
+import { Page } from "../../common/components/Page";
 import { AddCustomersViewModel } from "../../viewModels/AddcustomerViewModel";
 
 export function AddCustomerScreen() {
-    const theme = useTheme();
     const navigate = useNavigate();
-    const viewModel = AddCustomersViewModel();
+    const { customerFields, submit } = AddCustomersViewModel();
 
     function handleCancel() {
         navigate(-1);
     }
 
     return (
-        <Box
-            sx={{
-                display: "flex",
-                flexDirection: "column",
-                flex: 1,
-                background: theme.palette.action.hover,
-                borderRadius: theme.shape.borderRadius + "px",
-                m: 1,
-                p: 2,
-                pt: 1,
-            }}
-        >
-            <Typography variant="h6" fontWeight="semi-bold" mb={2}>
-                Clientes
-            </Typography>
-            <Box
-                component="div"
-                display="flex"
-                flexDirection="column"
-                maxWidth={900}
-            >
-                <Box component="form" flexDirection="column" mb={3}>
-                    <Typography variant="subtitle1">
-                        Informações pessoais
-                    </Typography>
-                    <Grid container spacing={1} mb={3}>
-                        <Grid item xs={12} sm={8}>
-                            <TextField
-                                variant="outlined"
-                                label="Nome"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.name}
-                            />
-                        </Grid>
-
-                        <Grid item xs={12} sm={4}>
-                            <TextField
-                                variant="outlined"
-                                label="CPF"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.cpf}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={8}>
-                            <TextField
-                                variant="outlined"
-                                label="email"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.email}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <TextField
-                                variant="outlined"
-                                label="Tefefone"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.telephone}
-                            />
-                        </Grid>
-                    </Grid>
-
-                    <Typography variant="subtitle1">Endereço</Typography>
-
-                    <Grid container spacing={1}>
-                        <Grid item xs={12} sm={8}>
-                            <TextField
-                                variant="outlined"
-                                label="Rua"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.street}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <TextField
-                                variant="outlined"
-                                label="Nº"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.houseNumber}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={8}>
-                            <TextField
-                                variant="outlined"
-                                label="Bairro"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.neighborhood}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <TextField
-                                variant="outlined"
-                                label="Cep"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.zipCode}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={8}>
-                            <TextField
-                                variant="outlined"
-                                label="Cidade"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.city}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <TextField
-                                variant="outlined"
-                                label="Estado"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.state}
-                            />
-                        </Grid>
-
-                        <Grid item xs={12} sm={4}>
-                            <TextField
-                                variant="outlined"
-                                label="Paíz"
-                                size="small"
-                                margin="dense"
-                                fullWidth
-                                {...viewModel.customerFields.country}
-                            />
-                        </Grid>
-                    </Grid>
-                </Box>
-                <Box display="flex" gap={3} justifyContent="end" mb={2}>
+        <Page title="Adicionar novo cliente">
+            <Form
+                submitButton={
                     <Button
-                        variant="outlined"
-                        size="small"
+                        type="submit"
+                        variant="contained"
+                        startIcon={<SaveRounded />}
+                        enableMinWidth
+                    >
+                        Salvar
+                    </Button>
+                }
+                cancelButton={
+                    <Button
                         startIcon={<CancelRounded />}
-                        sx={{ minWidth: 100 }}
+                        enableMinWidth
                         onClick={handleCancel}
                     >
                         Cancelar
                     </Button>
-                    <Button
-                        variant="contained"
-                        size="small"
-                        startIcon={<SaveRounded />}
-                        sx={{ minWidth: 100 }}
-                        onClick={viewModel.submit}
-                    >
-                        Salvar
-                    </Button>
-                </Box>
-            </Box>
-        </Box>
+                }
+                onSubmit={submit}
+            >
+                <FormFieldSet title="Informações pessoais">
+                    <FormInput
+                        label="Nome"
+                        xs={12}
+                        sm={8}
+                        fieldState={customerFields.name}
+                    />
+
+                    <FormInput
+                        label="CPF"
+                        xs={12}
+                        sm={4}
+                        fieldState={customerFields.cpf}
+                    />
+
+                    <FormInput
+                        label="email"
+                        xs={12}
+                        sm={8}
+                        fieldState={customerFields.email}
+                    />
+
+                    <FormInput
+                        label="Tefefone"
+                        xs={12}
+                        sm={4}
+                        fieldState={customerFields.telephone}
+                    />
+                </FormFieldSet>
+
+                <FormFieldSet title="Endereço">
+                    <FormInput
+                        label="Rua"
+                        xs={12}
+                        sm={8}
+                        fieldState={customerFields.street}
+                    />
+
+                    <FormInput
+                        label="Nº"
+                        xs={12}
+                        sm={4}
+                        fieldState={customerFields.houseNumber}
+                    />
+
+                    <FormInput
+                        label="Bairro"
+                        xs={12}
+                        sm={8}
+                        fieldState={customerFields.neighborhood}
+                    />
+
+                    <FormInput
+                        label="Cep"
+                        xs={12}
+                        sm={4}
+                        fieldState={customerFields.zipCode}
+                    />
+
+                    <FormInput
+                        label="Cidade"
+                        xs={12}
+                        sm={8}
+                        fieldState={customerFields.city}
+                    />
+
+                    <FormInput
+                        label="Estado"
+                        xs={12}
+                        sm={4}
+                        fieldState={customerFields.state}
+                    />
+
+                    <FormInput
+                        label="Paíz"
+                        xs={12}
+                        sm={4}
+                        fieldState={customerFields.country}
+                    />
+                </FormFieldSet>
+            </Form>
+        </Page>
     );
 }

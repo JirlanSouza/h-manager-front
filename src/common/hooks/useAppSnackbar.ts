1b2c7c9e466e137332f83c5e4b718f82f33240f3
Jirlan Souza <jirlansouza08@gmail.com>
2023-09-03 13:28:50 -0400

    feat: created the `useAppSnackbar` react hook

---
+++ src/common/hooks/useAppSnackbar.ts
@@ -0,0 +1,23 @@
+import { useSnackbar } from "notistack";
+const AUTO_RIDE_DURATION_ERROR_SNACKBAR = 10000;
+
+export function useAppSnackbar() {
+    const { enqueueSnackbar } = useSnackbar();
+    function enqueueSuccessSnackBar(message: string) {
+        enqueueSnackbar(message, {
+            variant: "success",
+        });
+    }
+
+    function enqueueErrorSnackBar(message: string) {
+        enqueueSnackbar(message, {
+            variant: "error",
+            autoHideDuration: AUTO_RIDE_DURATION_ERROR_SNACKBAR,
+        });
+    }
+
+    return {
+        enqueueSuccessSnackBar,
+        enqueueErrorSnackBar,
+    };
+}

ca66d7cc5ac338a31da9589055f9859f86c28a4e
Jirlan Souza <jirlansouza08@gmail.com>
2023-09-03 20:04:14 -0400

    refactor: change the `CustomerScreen` box components to `Page´ app component

--- src/screens/customers/CustomersScreen.tsx
+++ src/screens/customers/CustomersScreen.tsx
@@ -4,8 +4,10 @@ import {
     RemoveCircleRounded,
     ViewAgendaRounded,
 } from "@mui/icons-material";
-import { Box, Button, Typography, useTheme } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { Button } from "../../common/components/Button";
+import { Page } from "../../common/components/Page";
 import { NavigationPath } from "../../navigation/navigations";
 
 export function CustomersScreen() {
@@ -22,7 +24,7 @@ export function CustomersScreen() {
                 ? theme.palette.background.default
                 : theme.palette.action.hover;
 
-        return Array(20)
+        return Array(40)
             .fill(0)
             .map((_, i) => {
                 return (
@@ -39,54 +41,26 @@ export function CustomersScreen() {
     }
 
     return (
-        <Box
-            sx={{
-                display: "flex",
-                flexDirection: "column",
-                flex: 1,
-                background: theme.palette.action.hover,
-                borderRadius: theme.shape.borderRadius + "px",
-                m: 1,
-                p: 2,
-                pt: 1,
-            }}
-        >
-            <Typography variant="h6" fontWeight="semi-bold" mb={2}>
-                Clientes
-            </Typography>
+        <Page title="Clientes">
             <Box
                 component="div"
                 display="flex"
-                flex={1}
+                flexGrow={1}
                 flexDirection="column"
-                maxWidth={900}
+                maxWidth={theme.breakpoints.values.xl}
+                overflow="hidden"
             >
                 <Box display="flex" gap={1} justifyContent="end" mb={2}>
                     <Button
-                        variant="outlined"
-                        size="small"
                         sx={{ mr: "auto" }}
                         startIcon={<ViewAgendaRounded />}
                     >
                         Visualizar
                     </Button>
-                    <Button
-                        variant="outlined"
-                        size="small"
-                        startIcon={<EditRounded />}
-                    >
-                        Editar
-                    </Button>
-                    <Button
-                        variant="outlined"
-                        size="small"
-                        startIcon={<RemoveCircleRounded />}
-                    >
-                        Remover
-                    </Button>
+                    <Button startIcon={<EditRounded />}>Editar</Button>
+                    <Button startIcon={<RemoveCircleRounded />}>Remover</Button>
                     <Button
                         variant="contained"
-                        size="small"
                         startIcon={<AddRounded />}
                         onClick={() =>
                             handleNavigation(NavigationPath.CUSTOMERS_ADD)
@@ -100,11 +74,11 @@ export function CustomersScreen() {
                     component="div"
                     borderRadius={theme.shape.borderRadius + "px"}
                     border={`1px solid ${theme.palette.divider}`}
-                    overflow="hidden"
+                    overflow="auto"
                 >
                     {makeList()}
                 </Box>
             </Box>
-        </Box>
+        </Page>
     );
 }

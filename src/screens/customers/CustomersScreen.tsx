2d721efc7e711a3a017885b77daaa67c5c043d2a
Jirlan Souza <jirlansouza08@gmail.com>
2023-09-05 22:52:05 -0400

    feat: create the `Cusumer` summary table

--- src/screens/customers/CustomersScreen.tsx
+++ src/screens/customers/CustomersScreen.tsx
@@ -4,40 +4,38 @@ import {
     RemoveCircleRounded,
     ViewAgendaRounded,
 } from "@mui/icons-material";
-import { Box, useTheme } from "@mui/material";
+import {
+    Box,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow,
+    useTheme,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../common/components/Button";
 import { Page } from "../../common/components/Page";
 import { NavigationPath } from "../../navigation/navigations";
+import { useCustomerSummaryViewModel } from "../../viewModels/CustomerSummaryViewModel";
 
 export function CustomersScreen() {
     const theme = useTheme();
     const navigate = useNavigate();
+    const {
+        customers,
+        page,
+        itemsPerPage: itemsperPage,
+        totalItems,
+        selectedCustomer,
+        onSelectCustomer,
+    } = useCustomerSummaryViewModel();
 
     function handleNavigation(path: NavigationPath) {
         navigate(path);
-    }
-
-    function makeList() {
-        const background = (i: number) =>
-            i % 2
-                ? theme.palette.background.default
-                : theme.palette.action.hover;
-
-        return Array(40)
-            .fill(0)
-            .map((_, i) => {
-                return (
-                    <Box
-                        key={i}
-                        flex={1}
-                        sx={{
-                            height: theme.spacing(4),
-                            background: background(i),
-                        }}
-                    />
-                );
-            });
     }
 
     return (
@@ -70,14 +68,94 @@ export function CustomersScreen() {
                     </Button>
                 </Box>
 
-                <Box
-                    component="div"
-                    borderRadius={theme.shape.borderRadius + "px"}
-                    border={`1px solid ${theme.palette.divider}`}
-                    overflow="auto"
+                <Paper
+                    elevation={0}
+                    sx={{
+                        position: "relative",
+                        flex: 1,
+                        display: "flex",
+                        flexDirection: "column",
+                        height: "100%",
+                        overflow: "hidden",
+                    }}
                 >
-                    {makeList()}
-                </Box>
+                    <TableContainer sx={{ flex: 1 }}>
+                        <Table
+                            stickyHeader
+                            sx={{
+                                minWidth: 650,
+                            }}
+                            size="small"
+                        >
+                            <TableHead>
+                                <TableRow
+                                    sx={{
+                                        "& td, & th": {
+                                            borderBottom:
+                                                `1px solid ` +
+                                                theme.palette.divider,
+                                        },
+                                    }}
+                                >
+                                    <TableCell>Nome</TableCell>
+                                    <TableCell align="center">E-mail</TableCell>
+                                    <TableCell align="center">CPF</TableCell>
+                                    <TableCell align="right">
+                                        Cliente desde
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {customers.map((custumer, i) => (
+                                    <TableRow
+                                        key={custumer.id}
+                                        hover
+                                        selected={i === selectedCustomer}
+                                        sx={{
+                                            "& td, & th": {
+                                                borderBottom:
+                                                    `1px solid ` +
+                                                    theme.palette.divider,
+                                            },
+                                            "&:hover": {
+                                                cursor: "pointer",
+                                            },
+                                        }}
+                                        onClick={() => onSelectCustomer(i)}
+                                    >
+                                        <TableCell>{custumer.name}</TableCell>
+                                        <TableCell align="center">
+                                            {custumer.email}
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            {custumer.cpf}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {custumer.customerSince.toLocaleDateString(
+                                                "pt-br"
+                                            )}
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[10, 20, 25]}
+                        component="div"
+                        count={totalItems + 200}
+                        rowsPerPage={itemsperPage}
+                        page={page}
+                        showFirstButton
+                        showLastButton
+                        labelRowsPerPage="Items por página"
+                        labelDisplayedRows={({ from, to, count }) =>
+                            `${from} - ${to} de ${count}`
+                        }
+                        onPageChange={() => {}}
+                        onRowsPerPageChange={() => {}}
+                    />
+                </Paper>
             </Box>
         </Page>
     );

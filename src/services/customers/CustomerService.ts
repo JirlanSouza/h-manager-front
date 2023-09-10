d89abb0cf5869b6f10680a64e70cb524f34e269a
Jirlan Souza <jirlansouza08@gmail.com>
2023-09-09 22:09:28 -0400

    feat: created the `getSummary` method in the `CustomerService`

--- src/services/customers/CustomerService.ts
+++ src/services/customers/CustomerService.ts
@@ -1,5 +1,7 @@
 import { ApiGatway } from "../../common/api/ApiGatway";
+import { Page, Pageable } from "../../models/Pagination";
 import { CustomerData } from "../../models/customer/CustomerData";
+import { CustomerSummary } from "../../models/customer/CustomerSummary";
 
 export class CustomerService {
     constructor(private readonly apiGateway: ApiGatway) {}
@@ -8,6 +10,13 @@ export class CustomerService {
         return await this.apiGateway.post<CustomerData, string>(
             "/customers",
             customerData
+        );
+    }
+
+    async getSummary(pageable: Pageable) {
+        return await this.apiGateway.get<Page<CustomerSummary>>(
+            "/customers",
+            pageable
         );
     }
 }

89ec1577f89775099d0644515b9b5ee2cb0e8f4a
Jirlan Souza <jirlansouza08@gmail.com>
2023-09-10 13:30:04 -0400

    refactor: change the axios instaciate on `App` init metthod

--- src/App.ts
+++ src/App.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
 import ReactDOM from "react-dom/client";
 import { AppWrapper } from "./app/AppWrapper";
 import { ApiGatway } from "./common/api/ApiGatway";
+import { createAxiosInstance } from "./config/axiosConfig";
 import { CustomerServiceFactory } from "./services/customers/CusomerServiceFactory";
 import { CustomerService } from "./services/customers/CustomerService";
 
 export class App {
     init() {
-        const axiosInstace = axios.create({
-            baseURL: import.meta.env.VITE_API_BASE_URL,
-        });
+        const axiosInstace = createAxiosInstance(
+            import.meta.env.VITE_API_BASE_URL
+        );
         const apiGatway = new ApiGatway(axiosInstace);
         const customerService = new CustomerService(apiGatway);
         CustomerServiceFactory.setCustomerService(customerService);

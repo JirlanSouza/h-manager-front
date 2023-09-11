4e54202c659329e5824bbfcedb116ea2147c7e99
Jirlan Souza <jirlansouza08@gmail.com>
2023-09-10 20:33:28 -0400

    feat: added the `QueryClientProvider` in the `AppWrapper` component

--- src/app/AppWrapper.tsx
+++ src/app/AppWrapper.tsx
@@ -1,19 +1,23 @@
 import React, { JSX } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ColorModeContextProvider } from "../common/contexts/colorModeContext.tsx";
 import { AppRouter } from "../navigation/routes/AppRouter.tsx";
 import { SettingsService } from "../services/settingsService.ts";
 import { AppSnackbarProvider } from "./AppSnackbarProvider.tsx";
 
 const settingsService = new SettingsService();
+const queryClient = new QueryClient();
 
 export function AppWrapper(): JSX.Element {
     return (
         <React.StrictMode>
-            <ColorModeContextProvider settingsService={settingsService}>
-                <AppSnackbarProvider>
-                    <AppRouter settingsService={settingsService} />
-                </AppSnackbarProvider>
-            </ColorModeContextProvider>
+            <QueryClientProvider client={queryClient}>
+                <ColorModeContextProvider settingsService={settingsService}>
+                    <AppSnackbarProvider>
+                        <AppRouter settingsService={settingsService} />
+                    </AppSnackbarProvider>
+                </ColorModeContextProvider>
+            </QueryClientProvider>
         </React.StrictMode>
     );
 }

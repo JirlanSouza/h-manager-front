2aafa116a6c97b420b9d2a4defa4bc7c51b61915
Jirlan Souza <jirlansouza08@gmail.com>
2023-09-12 22:09:10 -0400

    feat: created the `FormCheckbox` app component

---
+++ src/common/components/FormCheckbox.tsx
@@ -0,0 +1,29 @@
+import { Breakpoint, Checkbox, FormControlLabel, Grid } from "@mui/material";
+import { FormFieldState } from "../hooks/FormState";
+
+type FormInputBreakpoints = Partial<Record<Breakpoint, number>>;
+
+type InputProps = {
+    label: string;
+    fieldState: FormFieldState;
+} & FormInputBreakpoints;
+
+export function FormCheckbox({
+    label,
+    fieldState,
+    xs,
+    sm,
+    md,
+    lg,
+    xl,
+}: InputProps) {
+    return (
+        <Grid item {...{ xs, sm, md, lg, xl }} sx={{ userSelect: "none" }}>
+            <FormControlLabel
+                label={label}
+                control={<Checkbox size="small" />}
+                {...fieldState}
+            />
+        </Grid>
+    );
+}

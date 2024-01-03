f31878b11077e6f3402f69325851a92df1dc34ea
Jirlan Souza <jirlansouza08@gmail.com>
2024-01-03 17:07:10 -0400

    feat: add the authentication routes

--- src/navigation/routes/AppRouter.tsx
+++ src/navigation/routes/AppRouter.tsx
@@ -1,14 +1,44 @@
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+    Outlet,
+    RouterProvider,
+    createBrowserRouter,
+    redirect,
+} from "react-router-dom";
 import { AppLayout } from "../../app/AppLayout";
+import { AuthenticationService } from "../../services/auth/AuthenticationService";
 import { SettingsService } from "../../services/settingsService";
 import { appNavigations } from "../navigations";
 
 interface AppRouterProps {
     settingsService: SettingsService;
+    authService: AuthenticationService;
 }
 
-export function AppRouter({ settingsService }: AppRouterProps) {
+export function AppRouter({ settingsService, authService }: AppRouterProps) {
     const router = createBrowserRouter([
+        {
+            path: "/login/authorize",
+            loader: async (loader) => {
+                const isAuthorized = await authService.getToken(
+                    new URL(loader.request.url)
+                );
+
+                if (isAuthorized) {
+                    return redirect("/");
+                }
+
+                return redirect("/login");
+            },
+            element: <></>,
+        },
+        {
+            path: "/login",
+            loader: () => {
+                authService.login();
+                return null;
+            },
+            element: <></>,
+        },
         {
             path: "/",
             element: (

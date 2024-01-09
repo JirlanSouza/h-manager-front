9a4de27dcaf0f12c80dbba59017f139411a81a66
Jirlan Souza <jirlansouza08@gmail.com>
2024-01-08 20:00:19 -0400

    refactor: change the theme mode handler to use the `ThemeModeService`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { AppBarComponent } from './layout/app-bar/app-bar.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), SidebarComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    SidebarComponent,
+    AppBarComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
